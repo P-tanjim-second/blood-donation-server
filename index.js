98f5d845f6413b9d6168392098b2e2be05011374
P-tanjim-second <moksedurtanjim2769@gmail.com>
2026-06-27 07:14:24 +0600

    connect database successfully

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    const db = client.db('blood-donation')
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
